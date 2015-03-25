3a26dd90377e34a13b07aaa79ce505add9e51b69
Krasimir Popov <kpopov@asteasolutions.com>
2015-03-25 23:44:16 +0200

    Adding compress method

--- week1/1-sparse-matrix/solution.rb
+++ week1/1-sparse-matrix/solution.rb
@@ -1,16 +1,21 @@
 #!/usr/bin/env ruby
 
+class Array
+  def compress
+    myarr = self
+    result = []
+    myarr.each do |el|
+      el.each do |val|
+        result.push(val) unless val.nil?
+      end
+    end
+    result
+  end
+end
+
 thearray = [[1, nil, nil, nil],
             [nil, 2, 5, nil],
             [6, nil, nil, 7],
             [nil, 3, nil, 4]]
 
-result = []
-
-thearray.each do |el|
-  el.each do |val|
-    result.push(val) unless val.nil?
-  end
-end
-
-p result
+p thearray.compress
