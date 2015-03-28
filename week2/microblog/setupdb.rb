38ad0a3a1ebc7aa5d8289c14f4aa46b80f4ad9dc
Krasimir Popov <kpopov@asteasolutions.com>
2015-03-28 17:07:00 +0200

    Add database support to myblog

---
+++ week2/microblog/setupdb.rb
@@ -0,0 +1,16 @@
+
+require 'sqlite3'
+
+db = SQLite3::Database.new "myblog.db"
+
+rows = db.execute <<-SQL
+  create table myposts (
+  	id INTEGER,
+    name varchar(30),
+    post varchar(255),
+    PRIMARY KEY(id)
+  );
+SQL
+
+db.execute("INSERT INTO myposts(name, post) VALUES (?, ?)", ['Dummyman', 'This is a dummu post'])
+db.execute("INSERT INTO myposts(name, post) VALUES (?, ?)", ['Dummyman1', 'This is a dummu post 2'])
--- week2/microblog/solution.rb
+++ week2/microblog/solution.rb
@@ -1,26 +1,32 @@
 
 require 'sinatra'
+require 'sqlite3'
 
-$main_storage = ['Some', 'Dummy', 'Posts']
+$db = SQLite3::Database.new "myblog.db"
 
 get '/' do
-  erb :viewall, :locals => { :posts => $main_storage }
+  erb :viewall, :locals => { :posts => $db }
 end
 
 get '/new' do
   '<form method="post">
    Post:<br>
-   <input type="text" name="message">
+   <input type="text" name="blogger" maxlength="8" size="8"><br>
+   <TEXTAREA name="message" maxlength="255" size="255" ROWS="15" COLS="25"></TEXTAREA><BR>
    <input type="submit" value="Submit">
    </form>'
 end
 
 get '/:id' do
-  erb :viewone, :locals => { :posts => $main_storage, :id => params[:id] }
+  erb :viewone, :locals => { :posts => $db, :id => params[:id] }
+end
+
+delete '/:id' do
+  'you can not delete your facebook'
 end
 
 post '/new' do
-  $main_storage.push(params[:message])
+  $db.execute("INSERT INTO myposts(name, post) VALUES (?, ?)", [params[:blogger], params[:message]])
   "You posted '#{params[:message]}'
   <meta http-equiv='refresh' content='1; url=/' />"
 end
