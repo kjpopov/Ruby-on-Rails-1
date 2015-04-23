6fb04e99a504f424c0fb345c0dd037f56c15f743
Krasimir Popov <kpopov@asteasolutions.com>
2015-04-23 21:57:18 +0300

    Add product model and products controller show action

--- week4/online-store/store/app/controllers/products_controller.rb
+++ week4/online-store/store/app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 # Controleri Mnojestveno chislo, Modeli edinstveno
 class ProductsController < ApplicationController
   def index
+    render json: Product.all
+  end
+
+  def show
+    render json: Product.find(params[:id])
   end
 end
