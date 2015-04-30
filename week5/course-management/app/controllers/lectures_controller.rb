aa30ada4027d9e14f3d9b4bb0269aa991559f77a
Krasimir Popov <kpopov@asteasolutions.com>
2015-04-30 21:39:24 +0300

    Adding CRUD options to lectures

--- week5/course-management/app/controllers/lectures_controller.rb
+++ week5/course-management/app/controllers/lectures_controller.rb
@@ -1,6 +1,65 @@
 class LecturesController < ApplicationController
 
-def index
-end
+before_action :set_lecture, only: [:show, :edit, :update, :destroy]
+
+  def index
+	  @lectures = Lecture.all
+  end
+
+  def show
+  end
+
+  def new
+    @lecture = Lecture.new
+  end
+
+  def edit
+  end
+
+  def create
+    @lecture = Lecture.new(lecture_params)
+
+    respond_to do |format|
+      if @lecture.save
+        format.html { redirect_to @lecture, notice: 'lecture was successfully created.' }
+        format.json { render :show, status: :created, location: @lecture }
+      else
+        format.html { render :new }
+        format.json { render json: @lecture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    respond_to do |format|
+      if @lecture.update(lecture_params)
+        format.html { redirect_to @lecture, notice: 'lecture was successfully updated.' }
+        format.json { render :show, status: :ok, location: @lecture }
+      else
+        format.html { render :edit }
+        format.json { render json: @lecture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @lecture.destroy
+    respond_to do |format|
+      format.html { redirect_to lectures_url, notice: 'This Lecture was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_lecture
+    @lecture = Lecture.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def lecture_params
+    params.require(:lecture).permit(:id, :name, :description)
+  end
 
 end
--- week5/course-management/app/models/lecture.rb
+++ week5/course-management/app/models/lecture.rb
@@ -1,3 +1,5 @@
 class Lecture < ActiveRecord::Base
+	validates :name, presence: true,
+            length: { minimum: 5 }
 	has_many :tasks
 end
